cf9b65a1c6b2438020cca42cfc54791a8e543829
Bruno323UL <103434759+Bruno323UL@users.noreply.github.com>
2022-04-21 12:50:34 -0300

    Add files via upload

--- logica.js
+++ logica.js
@@ -1,9 +1,22 @@
-let nombre = "Elbruno"
-let edad = 24; 
-console.log (edad)
-prompt("Hola gente")
-alert("uenas")
-let mensaje = prompt("escriba un numero")
-console.log(mensaje);
-nombre = 20;
-console.log(nombre)
+var numer1;
+var numer2;
+var suma;
+
+
+numer1 = parseFloat(prompt("ingrese un valor"))
+numer2 = parseFloat(prompt("ingrese un valor"))
+
+
+
+
+
+//let nombre = "Elbruno"
+//let edad = 24; 
+//console.log (edad)
+//prompt("Hola gente")
+//alert("uenas")
+//(let mensaje = prompt("escriba un numero")
+//console.log(mensaje);
+//nombre = 20;
+//console.log(nombre)
+
