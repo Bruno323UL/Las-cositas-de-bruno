1f9b2e25074e510c2dac34c68bbf78ae02e29be2
Bruno323UL <103434759+Bruno323UL@users.noreply.github.com>
2022-04-21 12:31:41 -0300

    Add files via upload

---
+++ logica.js
@@ -0,0 +1,9 @@
+let nombre = "Elbruno"
+let edad = 24; 
+console.log (edad)
+prompt("Hola gente")
+alert("uenas")
+let mensaje = prompt("escriba un numero")
+console.log(mensaje);
+nombre = 20;
+console.log(nombre)
